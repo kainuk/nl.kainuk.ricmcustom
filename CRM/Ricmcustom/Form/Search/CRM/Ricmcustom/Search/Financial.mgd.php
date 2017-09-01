5efa655f96f68f5f75d77b5d7b48d3fb1dc68425
Klaas Eikelboom <klaas.eikelboom@civicoop.org>
2017-09-01 12:05:38 -0400

    Generated reports

---
+++ CRM/Ricmcustom/Form/Search/CRM/Ricmcustom/Search/Financial.mgd.php
@@ -0,0 +1,19 @@
+<?php
+// This file declares a managed database record of type "CustomSearch".
+// The record will be automatically inserted, updated, or deleted from the
+// database as appropriate. For more details, see "hook_civicrm_managed" at:
+// http://wiki.civicrm.org/confluence/display/CRMDOC42/Hook+Reference
+return array (
+  0 => 
+  array (
+    'name' => 'CRM_Ricmcustom_Form_Search_CRM_Ricmcustom_Search_Financial',
+    'entity' => 'CustomSearch',
+    'params' => 
+    array (
+      'version' => 3,
+      'label' => 'CRM_Ricmcustom_Search_Financial',
+      'description' => 'CRM_Ricmcustom_Search_Financial (nl.kainuk.ricmcustom)',
+      'class_name' => 'CRM_Ricmcustom_Form_Search_CRM_Ricmcustom_Search_Financial',
+    ),
+  ),
+);
